3f8854e9ca1ff4c9504fd3539fc71dfbe217d2c7
KlaasHagge <klaash@ppedv.de>
2022-02-10 11:38:34 +0100

    Validation

---
+++ Validation/ValidationMarkup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Validation
+{
+    internal class ValidationMarkup : MarkupExtension
+    {
+        private Binding binding;
+
+        public ValidationMarkup(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            foreach (var item in validationRules)
+            {
+                binding.ValidationRules.Add(item);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+
+
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
+}
