fbaea1ea04dd847fab1cc2f01da25d8af98b8985
KlaasHagge <klaash@ppedv.de>
2022-02-11 10:00:36 +0100

    Neues Fenster

---
+++ Personendatenbank/PersonToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Threading.Tasks;
+
+namespace Personendatenbank
+{
+    public class PersonToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Person;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
